b2b184fcd359c8bfa06bef05108374357475974e
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-27 12:59:27 -0400

    Added properties to customer and employee models.py

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -7,3 +7,12 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=5)
+    balance = models.IntegerField()
+    weekly_pickup_day = models.CharField(max_length=10)
+    one_time_pickup = models.DateField()
+    suspend_start = models.DateField(null=True)
+    suspend_end = models.DateField(null=True)
+    suspend_status = models.BooleanField(null=True)
+    pickup_status = models.BooleanField()
--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 
 # TODO: Create an Employee model with properties required by the user stories
+
+class Employee(models.Model):
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=5)
