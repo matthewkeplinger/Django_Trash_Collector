c07881d18d86c8be5e61f1d70f1405e13a7ba072
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-09-01 14:35:19 -0400

    Fixed acct registration; need to push acct-details to index

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 app_name = "employees"
 urlpatterns = [
-    path('', views.daily_view, name="index"),
+    path('', views.index, name="index"),
     path('create/', views.create, name="create"),
     path('daily_view/', views.daily_view, name='daily_view'),
     path('day_filter/', views.choose_day, name='day_filter'),
